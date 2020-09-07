c2d3c2693cc5007c1514b0f271c27b83a32fdc2a
Farias Maiquita <fmaiquita@gmail.com>
2020-09-07 20:14:34 +0200

    Require redux config, site header widget, Add Redux Framework to required plugins

--- functions.php
+++ functions.php
@@ -2,6 +2,13 @@
 
 // Required Files
 require_once get_template_directory() . '/_inc/classes/class-tgm-plugin-activation.php';
+require_once get_template_directory() . '/_inc/partials/redux-config.php';
+
+
+// Require Custom Elementor Widgets
+add_action( 'elementor/widgets/widgets_registered', function() {
+    require_once( get_template_directory() . '/_inc/widgets/elementor/site-header.php' );
+});
 
 
 // Register Required Plugins
@@ -11,13 +18,16 @@ add_action( 'tgmpa_register', function () {
             'name'      => 'Elementor Page Builder',
             'slug'      => 'elementor',
             'required'  => true
-        ],
-        [
+        ], [
             'name'      => 'Elementor – Header, Footer & Blocks Template',
             'slug'      => 'header-footer-elementor',
             'required'  => true
+        ], [
+            'name'      => 'Redux – Gutenberg Blocks Library & Framework',
+            'slug'      => 'redux-framework',
+            'required'  => true
         ]
-    ],[
+    ], [
         'id'           => 'wgp',
         'default_path' => '',
         'menu'         => 'tgmpa-install-plugins',
@@ -46,16 +56,29 @@ add_action( 'after_setup_theme', function() {
 
     add_theme_support(
         'html5',
-        array(
+        [
             'search-form',
             'comment-form',
             'comment-list',
             'gallery',
             'caption',
             'script',
-            'style',
-        )
+            'style'
+        ]
     );
+});
+
+
+// Categories for Custom Elementor Widgets
+add_action( 'elementor/init', function() {
+    \Elementor\Plugin::$instance->elements_manager->add_category('wgp_header_footer', ['title' => __( 'WGP Header & Footer', 'wgp' )]);
+});
+
+
+// Icon CSS for Custom Elementor Widgets
+add_action( 'elementor/editor/before_enqueue_scripts', function() {
+    $theme_version = wp_get_theme()->get( 'Version' );
+    wp_enqueue_style( 'wgp-widget-icons', get_template_directory_uri() . '/_inc/assets/admin/widgets-icon.css', [], $theme_version );
 });
 
 
@@ -64,13 +87,13 @@ add_action( 'wp_enqueue_scripts', function () {
     $theme_version = wp_get_theme()->get( 'Version' );
 
     // Styles
-    wp_enqueue_style( 'wgp-style', get_stylesheet_uri(), array(), $theme_version );
+    wp_enqueue_style( 'wgp-style', get_stylesheet_uri(), [], $theme_version );
     // wp_add_inline_style( 'o-style', o_get_customizer_css( 'front-end' ) );
 
     // Scripts
     if ( ( ! is_admin() ) && is_singular() && comments_open() && get_option( 'thread_comments' ) )
         wp_enqueue_script( 'comment-reply' );
-    wp_enqueue_script( 'wgp-script', get_template_directory_uri() . '/_inc/assets/js/script.js', array(), $theme_version, true );
+    wp_enqueue_script( 'wgp-script', get_template_directory_uri() . '/_inc/assets/js/script.js', [], $theme_version, true );
     // wp_script_add_data( 'o-script', 'async', true );
 });
 
