051743927540fd95114d5d423b927da250e3ee7f
Farias Maiquita <fmaiquita@gmail.com>
2020-09-11 15:44:52 +0200

    Add ask for a quote button markup and css files

---
+++ _inc/partials/btn-ask-quote.php
@@ -0,0 +1,15 @@
+<?php // Partial - Ask for a Quote Button
+
+// Using the Redux API
+//echo Redux::get_option( 'OPT_NAME', 'FIELD_ID', 'DEFAULT_VALUE' );
+
+// Using the global argment
+//global $redux_demo; // Same as your opt_name
+//echo $redux_demo['FIELD_ID'];
+
+?>
+
+<a href="#" class="btn-ask-quote btn-primary">
+    <span class="btn-text">Ask for a Quote</span>
+    <svg><use xlink:href="#arrow-icon"></svg>
+</a>
