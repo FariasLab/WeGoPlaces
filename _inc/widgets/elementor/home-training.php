33cd0ce67f3c5daab021d2e656af88571f34cf64
Farias Maiquita <fmaiquita@gmail.com>
2020-09-13 22:46:12 +0200

    Update home training widget and its css

--- _inc/widgets/elementor/home-training.php
+++ _inc/widgets/elementor/home-training.php
@@ -24,84 +24,15 @@ class WGP_Home_Training extends Widget_Base
     }
 
     protected function _register_controls() {
-
-        $this->start_controls_section(
-            'content_section', [
-                'label' => __('Content', 'wgp'),
-                'label_block' => true,
-                'tab' => Controls_Manager::TAB_CONTENT,
-            ]
-        );
-
-        $this->add_control(
-            'section_tagline', [
-                'label' => __( 'Section Tagline', 'wgp' ),
-                'label_block' => true,
-                'type' => Controls_Manager::TEXTAREA,
-                'rows' => 2
-            ]
-        );
-
-        $this->add_control(
-            'section_title', [
-                'label' => __( 'Section Title', 'wgp' ),
-                'label_block' => true,
-                'type' => Controls_Manager::TEXTAREA,
-                'rows' => 2
-            ]
-        );
-
-        $this->add_control(
-            'icon1_text', [
-                'label' => __( 'Icon 1 Text', 'wgp' ),
-                'label_block' => true,
-                'type' => Controls_Manager::TEXT
-            ]
-        );
-
-        $this->add_control(
-            'icon2_text', [
-                'label' => __( 'Icon 2 Text', 'wgp' ),
-                'label_block' => true,
-                'type' => Controls_Manager::TEXT
-            ]
-        );
-
-        $this->add_control(
-            'icon3_text', [
-                'label' => __( 'Icon 3 Text', 'wgp' ),
-                'label_block' => true,
-                'type' => Controls_Manager::TEXT
-            ]
-        );
-
-        $this->add_control(
-            'button_text', [
-                'label' => __( 'Button Text', 'wgp' ),
-                'label_block' => true,
-                'type' => Controls_Manager::TEXT
-            ]
-        );
-
-        $this->add_control(
-            'button_link', [
-            'label' => __( 'Button Link', 'wgp' ),
-            'type' => Controls_Manager::URL,
-            'show_external' => false
+        register_common_controls($this, [
+            ['section_tagline', 'Section Tagline', Controls_Manager::TEXTAREA],
+            ['section_title', 'Section Title', Controls_Manager::TEXTAREA],
+            ['icon1_text', 'Icon 1 Text', Controls_Manager::TEXT],
+            ['icon2_text', 'Icon 2 Text', Controls_Manager::TEXT],
+            ['icon3_text', 'Icon 3 Text', Controls_Manager::TEXT],
+            ['button_text', 'Button Text', Controls_Manager::TEXT],
+            ['button_link', 'Button Link', Controls_Manager::URL]
         ]);
-
-        $this->add_control(
-            'help', [
-                'label' => __( 'To edit other content:', 'wgp' ),
-                'type' => Controls_Manager::RAW_HTML,
-                'label_block' => true,
-                'separator' => 'before',
-                'raw' => '<br>' . __( 'Please contact your web developer.', 'wgp' )
-            ]
-        );
-
-        $this->end_controls_section();
-
     }
 
     protected function render() {
@@ -125,10 +56,12 @@ class WGP_Home_Training extends Widget_Base
                     <img src="<?php bloginfo('template_url'); ?>/_inc/assets/img/home-training.svg" class="section-img">
                 </div>
                 <ul class="icon-list">
-                    <?php foreach ($icon_list as $item) { ?>
+                    <?php foreach ($icon_list as $item) {
+                        $img_src = get_template_directory_uri() . '/_inc/assets/img/icon-' . $item['icon'] . '.svg';
+                        $img_class = 'icon-img icon-' . $item['icon']; ?>
                         <li class="icon-list-item">
                             <div class="icon-wrap">
-                                <img src="<?php bloginfo('template_url'); echo '/_inc/assets/img/icon-' . $item['icon'] . '.svg'; ?>" class="icon-img">
+                                <img src="<?php echo $img_src; ?>" class="<?php echo $img_class; ?>">
                             </div>
                             <div class="icon-text"><?php echo $item['text']; ?></div>
                         </li>
