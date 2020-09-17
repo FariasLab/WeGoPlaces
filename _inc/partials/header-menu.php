4771115555767b897b29344b81a2238dae334469
Farias Maiquita <fmaiquita@gmail.com>
2020-09-17 08:23:53 +0200

    Add partials for header menu and lang switcher

---
+++ _inc/partials/header-menu.php
@@ -0,0 +1,8 @@
+<?php // Partial - Header Menu
+
+wp_nav_menu([
+    'menu_class' => 'menu header-menu',
+    'container' => '',
+    'depth' => 1,
+    'theme_location' => 'header'
+]);
---
+++ _inc/partials/lang-switcher.php
@@ -0,0 +1,14 @@
+<?php // Partial - Language Switcher Menu
+
+/*wp_nav_menu([
+'menu_class' => 'menu lang-switcher',
+'container' => '',
+'depth' => 1,
+'theme_location' => 'lang_switcher'
+]);*/
+
+// Temp Lang Switcher for Testing ?>
+<ul class="menu lang-switcher">
+    <li class="current-menu-item"><a href="#">Eng</a></li>
+    <li><a href="#">Por</a></li>
+</ul>
