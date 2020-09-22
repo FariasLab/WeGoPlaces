475a21052422dd8d0ce5e6028c668fd183b81ce2
Farias Maiquita <fmaiquita@gmail.com>
2020-09-22 12:35:11 +0200

    Add translation hero widget and its sass

---
+++ _inc/widgets/elementor/translation-hero.php
@@ -0,0 +1,58 @@
+<?php // Translation Hero Elementor Widget
+
+namespace Elementor;
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class WGP_Translation_Hero extends Widget_Base
+{
+
+    public function get_name() {
+        return 'wgp_translation_hero';
+    }
+
+    public function get_title() {
+        return __('Translation Hero', 'wgp');
+    }
+
+    public function get_icon() {
+        return 'wgp-widget-icon';
+    }
+
+    public function get_categories() {
+        return ['wgp_translation_page'];
+    }
+
+    protected function _register_controls() {
+        register_common_controls($this, [
+            ['section_tagline', 'Section Tagline', Controls_Manager::TEXTAREA],
+            ['section_title', 'Section Title', Controls_Manager::TEXTAREA],
+            ['body_text', 'Body Text', Controls_Manager::TEXTAREA, ['rows' => 6]]
+        ]);
+    }
+
+    protected function render() {
+
+        $settings = $this->get_settings_for_display();
+
+        if ( Plugin::$instance->editor->is_edit_mode() || Plugin::$instance->preview->is_preview_mode() ) {
+            get_template_part('_inc/partials/icon-svg-symbols');
+        } ?>
+
+        <section class="wgp-translation-hero">
+            <div class="inner-wrap">
+                <p class="section-tagline"><?php echo $settings['section_tagline']; ?></p>
+                <h2 class="section-title"><?php echo $settings['section_title']; ?></h2>
+                <div class="section-img-wrap">
+                    <img src="<?php bloginfo('template_url'); ?>/_inc/assets/img/translation-hero.svg" class="section-img">
+                </div>
+                <p class="body-text"><?php echo $settings['body_text']; ?></p>
+                <?php get_template_part('_inc/partials/btn-ask-quote'); ?>
+            </div>
+        </section>
+
+    <?php }
+
+}
+
+Plugin::instance()->widgets_manager->register_widget_type( new WGP_Translation_Hero() );
