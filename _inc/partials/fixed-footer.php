292b58f9f4851d2ce0800b0a14ea4d3289fb6cb2
Farias Maiquita <fmaiquita@gmail.com>
2020-10-05 13:08:02 +0200

    Add dynamic text to fixed footer and minor refactor

--- _inc/partials/fixed-footer.php
+++ _inc/partials/fixed-footer.php
@@ -1,13 +1,15 @@
-<?php // Partial - Fixed Footer ?>
+<?php // Partial - Fixed Footer
 
-<div class="fixed-footer">
+global $WGP; ?>
+
+<div class="wgp-fixed-footer">
     <?php get_template_part('_inc/partials/btn-ask-quote'); ?>
     <section class="consent-section">
         <p class="consent-wrap inner-wrap">
             <a href="#" class="btn-close-consent">
                 <svg class="close-icon"><use xlink:href="#close-icon"></svg>
             </a>
-            <span class="consent-text">By using our website you agree to our use of cookies to deliver a better experience</span>
+            <span class="consent-text"><?php echo $WGP['cookie_consent_text']; ?></span>
             <a href="#" class="btn-ok-consent">OK</a>
         </p>
     </section>
